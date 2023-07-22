e96747a7f641e5c42ab8e55f45aedbb77f8f8bbb
arun-arunisto <arun.arunisto2@gmail.com>
2023-07-22 18:35:05 +0530

    candidate auth

--- internshipProgram/urls.py
+++ internshipProgram/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('candidate_signup/', views.candidate_signup, name="candidate_signup"),
     path('candidate_login/', views.candidate_login, name="candidate_login"),
     path('application_form/', views.application_form, name="application_form"),
-    path('application_status/', views.application_status, name="application_status")
+    path('application_status/', views.application_status, name="application_status"),
+    path('candidate_logout/', views.candidate_logout, name="candidate_logout")
 ]
--- internshipProgram/views.py
+++ internshipProgram/views.py
@@ -1,17 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 def candidate_signup(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.get(email=email)
+            messages.error(request, "Email already registered!")
+        except User.DoesNotExist:
+            try:
+                applicant = User.objects.create_user(username=username,
+                                                     email=email,
+                                                     password=password)
+                applicant.save()
+                messages.info(request, "Registered Successfully!!")
+                return redirect('candidate_login')
+            except:
+                messages.error(request, "Username already registered!")
     return render(request, "candidate_signup.html")
 
 def candidate_login(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, "Login Successfully!!")
+            return redirect('home')
+        messages.error(request, "Email/Password incorrect!!")
     return render(request, "candidate_login.html")
 
+@login_required
 def application_form(request):
     return render(request, "application_form.html")
 
+@login_required
 def application_status(request):
     return render(request, "application_status.html")
+
+@login_required
+def candidate_logout(request):
+    logout(request)
+    return redirect('home')
